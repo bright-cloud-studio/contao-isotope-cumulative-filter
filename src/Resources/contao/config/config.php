f3e1e58383edcdd3d8e3c73a35789c0d08721532
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-03-13 13:18:10 -0400

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,9 @@
+<?php
+ 
+/**
+* @copyright  Bright Cloud Studio
+* @author     Bright Cloud Studio
+*/
+
+/** Hooks */
+//$GLOBALS['TL_HOOKS']['generatePage'][] 		 = array('Bcs\Hooks', 'commentedOutHook');
