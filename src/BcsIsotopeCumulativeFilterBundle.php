9d5aada4f298a75bd6c97a31b168d0cbc2efcd07
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-03-13 13:16:26 -0400

    Create BcsIsotopeCumulativeFilterBundle.php

---
+++ src/BcsIsotopeCumulativeFilterBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao Isotope Cumulative Filter
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-isotope-cumulative-filter
+ */
+
+namespace Bcs\IsotopeCumulativeFilterBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsIsotopeCumulativeFilterBundle extends Bundle
+{
+}
