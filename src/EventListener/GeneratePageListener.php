837ff2da70a7f794a5d26e8a5eeb787dedfc6476
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-03-18 15:27:12 -0400

    Update GeneratePageListener.php

--- src/EventListener/GeneratePageListener.php
+++ src/EventListener/GeneratePageListener.php
@@ -11,7 +11,7 @@ use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
 #[AsHook('generatePage')]
 class GeneratePageListener
 {
-    public function __invoke(PageModel $pageModel, LayoutModel $layout, PageRegular $pageRegular): void
+    public function __invoke(PageModel $pageModel, LayoutModel $layout, object $pageRegular): void
     {
         // Check if the request contains 'isorc' or 'cumulativefilter' parameters
         if (preg_match('/[?&](isorc|cumulativefilter)=/', Environment::get('request'))) {
