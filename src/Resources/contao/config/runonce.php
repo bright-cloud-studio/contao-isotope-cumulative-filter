ba4b81d61560c9d986e1315c0c91cd61b546432b
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-03-18 14:48:18 -0400

    Delete src/Resources/contao/config/runonce.php

--- src/Resources/contao/config/runonce.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db = \Contao\Database::getInstance();
-
-if (!$db->fieldExists('config_hash', 'tl_iso_request_cache')) {
-    $db->execute(
-        "ALTER TABLE tl_iso_request_cache ADD config_hash VARCHAR(12) NOT NULL DEFAULT ''"
-    );
-}
