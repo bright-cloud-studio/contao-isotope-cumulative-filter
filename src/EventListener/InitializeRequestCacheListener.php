2a4de81f6355882428f7596f745a9bc2295a632c
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-03-19 12:49:03 -0400

    Update InitializeRequestCacheListener.php

--- src/EventListener/InitializeRequestCacheListener.php
+++ src/EventListener/InitializeRequestCacheListener.php
@@ -69,7 +69,7 @@ class InitializeRequestCacheListener
             // so the URL is clean and Isotope does not see an invalid cache entry.
             $cleanUrl = Url::removeQueryString(['isorc']);
 
-            header('HTTP/1.1 302 Found');
+            header('HTTP/1.1 301 Moved Permanently');
             header('Location: ' . Environment::get('base') . ltrim($cleanUrl, '/'));
             exit;
         }
