3eff8331061b6f701418680019a65f3c73d71aa5
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-03-19 11:37:43 -0400

    Update BcsIsotopeCumulativeFilterBundle.php

--- src/BcsIsotopeCumulativeFilterBundle.php
+++ src/BcsIsotopeCumulativeFilterBundle.php
@@ -1,23 +1,17 @@
 <?php
 
 /**
- * @copyright  Bright Cliud Studio
+ * @copyright  Bright Cloud Studio
  * @author     Bright Cloud Studio
  * @package    Contao Isotope Cumulative Filter
  * @license    LGPL-3.0+
- * @see	       https://github.com/bright-cloud-studio/contao-isotope-cumulative-filter
+ * @see        https://github.com/bright-cloud-studio/contao-isotope-cumulative-filter
  */
 
 namespace Bcs\IsotopeCumulativeFilterBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
-use Bcs\IsotopeCumulativeFilterBundle\DependencyInjection\BcsIsotopeCumulativeFilterExtension;
 
 class BcsIsotopeCumulativeFilterBundle extends Bundle
 {
-    public function getContainerExtension(): ?ExtensionInterface
-    {
-        return new BcsIsotopeCumulativeFilterExtension();
-    }
 }
