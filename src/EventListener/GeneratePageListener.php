9dfd6508cd380da33b077c428b91e9d043d5de82
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-03-19 11:48:52 -0400

    Update GeneratePageListener.php

--- src/EventListener/GeneratePageListener.php
+++ src/EventListener/GeneratePageListener.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao Isotope Cumulative Filter
+ * @license    LGPL-3.0+
+ * @see        https://github.com/bright-cloud-studio/contao-isotope-cumulative-filter
+ */
+
 namespace Bcs\IsotopeCumulativeFilterBundle\EventListener;
 
 use Contao\PageModel;
